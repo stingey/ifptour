7dbb89d40e7251b6f5fa250e9967e43d83ef8c0d
stingey <kenopony@gmail.com>
2018-01-09 00:17:58 -0700

    fix navbar with bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,4 @@
 //= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
-//= require bootstrap
 //= require_tree .
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,1 @@
-$(document).on("scroll", function () {
-  if ($(document).scrollTop() > 22) {
-    $('.navbar-main').addClass('fix');
-    $('.navbar-filler').show();
-  } else {
-    $('.navbar-filler').hide();
-    $('.navbar-main').removeClass('fix');
-  }
-}.bind(this));
+
