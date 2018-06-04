903cebe64c3748de66e29e670fe9612b3f0ec9e3
stingey <kenopony@gmail.com>
2018-06-04 08:53:36 -0600

    fix search and filters

--- app/controllers/rankings/singles_controller.rb
+++ app/controllers/rankings/singles_controller.rb
@@ -4,7 +4,7 @@ module Rankings
       players = Player.all
       if params[:term].present?
         @search = true
-        players_result = players.where("first_name || ' ' || last_name LIKE ?", "%#{params[:term]}%")
+        players_result = players.where("first_name || ' ' || last_name ILIKE ?", "%#{params[:term]}%")
                                 .joins(:ranking_detail)
                                 .order('ranking_details.singles_points desc')
       else
@@ -12,7 +12,7 @@ module Rankings
                                 .order('ranking_details.singles_points desc')
       end
       if params[:rank_class].present?
-        players_result = players_result.joins(:ranking_detail).where('ranking_details.singles_rank == ?', params[:rank_class])
+        players_result = players_result.joins(:ranking_detail).where('ranking_details.singles_rank = ?', params[:rank_class])
       end
       @players = players_hash(players_result)
     end
